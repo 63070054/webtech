8ecb5f2bd4c9fc86d4b71be79638e30de7f570c5
ntp.vv <63070054@kmitl.ac.th>
2021-10-15 20:56:45 +0700

    Change Structure

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php include './link/all_link.php'?>
+</head>
+
+<body>
+    <div class="row">
+        <div class="col-6">
+            test
+        </div>
+        <div class="col-6">
+            test
+        </div>
+    </div>
+    <div class="test" data-aos="zoom-in">
+    </div>
+</body>
+<script>
+    AOS.init({
+        duration: 1200,
+    });
+</script>
+
+</html>
