43b430689a75024c446e8977a6640a4934953574
63070134 <73682774+63070134@users.noreply.github.com>
2021-10-17 16:06:29 +0700

    ข้าวสารรรรรรรร

---
+++ news.php
@@ -0,0 +1,77 @@
+<html lang="en">
+<head>
+    <?php include './link/all_link.php' ?>
+</head>
+
+<body>
+<?php
+    $nav_hero_active = 0;
+    $nav_news_active = 1;
+    include './components/nav.php';
+?>
+    <div class="all-news-title" style="background-image: url('https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/3703047/44a06189990aecb48c7c5cad5bf4d6d12ab72f2b.png');width: 100%;height: 600px;">
+        <div class="shadow-news" style="background: linear-gradient(rgba(0, 0, 0, 0) 60%,rgba(0, 0, 0, 0.733) 70%,rgb(0, 0, 0) 90%);"></div>
+        <div class="shadow-news" style="background: linear-gradient(rgba(0, 0, 0, 0) 60%,rgba(0, 0, 0, 0.733) 70%,rgb(0, 0, 0) 90%);"></div>
+    </div>
+    <div class="line-news-update">
+        <div class="table-news-update">
+            <a href="#" class="text-news-update">ข่าวสาร</a>
+            <a href="#" class="text-news-update">อัฟเดต</a>
+        </div>
+    </div>
+
+    <div style="background-image: url(https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react//blog/bg_repeater.jpg);width: 100%;height:100%;">
+        <div class="table-allnews">
+            <a style="width: 400px; height: 250px; margin: 15px;" href="#">
+                <div class="news" style="background-image: url(&quot;https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/3703047/aa9c5cc55358ebc6977e895d5d9c50ecaa718482.png&quot;);">
+                    <div class="shadow-news"></div>
+                    <div class="news-content-hidden">For fans of DOTA: Dragon's Blood, the next hero joining the battle of the Ancients is already a familiar face.</div>
+                    <div class="news-title">This Fall Marci Joins the Fight</div>
+                    <div class="news-date">16 ตุลาคม 2564</div>
+                </div>
+            </a>
+            <a style="width: 400px; height: 250px; margin: 15px;" href="#">
+                <div class="news" style="background-image: url(&quot;https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/3703047/7c5a93d3e497813ba9065b2a32d0b14f071a1217.png&quot;);">
+                    <div class="shadow-news"></div>
+                    <div class="news-content-hidden">งานแข่งหลัก The International จะเริ่มต้นขึ้นที่สนาม National Arena ในกรุงบูคาเรสต์ในอีกไม่ถึง 12 ชั่วโมง ร่วมรับชมตั้งแต่เวลา 10:00 EEST (14:00 น. เวลาประเทศไทย) ทางเว็บไซต์ DPC ทางไคลเอนต์ Dota หรือทาง Twitch </div>
+                    <div class="news-title">งานแข่งหลัก The International</div>
+                    <div class="news-date">12 ตุลาคม 2564</div>
+                </div>
+            </a>
+            <a style="width: 400px; height: 250px; margin: 15px;" href="#">
+                <div class="news" style="background-image: url(&quot;https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/3703047/a3a0b19b7e25c97584c764e62a113e7194010b49.png&quot;);">
+                    <div class="shadow-news"></div>
+                    <div class="news-content-hidden">การมอบพื้นที่ให้สตูดิโอภายนอกและนักแคสต์รายบุคคลได้มีโอกาสเชื่อมต่อกับผู้ชมเฉพาะกลุ่มนั้นเป็นสิ่งยอดเยี่ยมสำหรับทุกคนที่รัก Dota และ The International...</div>
+                    <div class="news-title">การปรับเปลี่ยนกฎสำหรับการสตรีม</div>
+                    <div class="news-date">9 ตุลาคม 2564</div>
+                </div>
+            </a>
+            <a style="width: 400px; height: 250px; margin: 15px;" href="#">
+                <div class="news" style="background-image: url(&quot;https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/3703047/a3a0b19b7e25c97584c764e62a113e7194010b49.png&quot;);">
+                    <div class="shadow-news"></div>
+                    <div class="news-content-hidden">ขณะนี้เราเห็นว่ามีความสับสนเกี่ยวกับข้อกำหนดสำหรับการถ่ายทอดสดของชุมชนในงาน TI10เช่นเดียวกับปีก่อน ๆ ใครก็สามารถทำการถ่ายทอดสดของชุมชนได้ หาก (1) อ้างอิงการถ่ายทอดสดนั้นจาก...</div>
+                    <div class="news-title">การถ่ายทอดสดของชุมชนสำหรับงาน The International</div>
+                    <div class="news-date">8 ตุลาคม 2564</div>
+                </div>
+            </a>
+            <a style="width: 400px; height: 250px; margin: 15px;" href="#">
+                <div class="news" style="background-image: url(&quot;https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/3703047/c1bc2216355cc4e326102d2af2c570ca5185dfb4.png&quot;);">
+                    <div class="shadow-news"></div>
+                    <div class="news-content-hidden">เฉลิมฉลองการกลับมาของ The International ด้วยการซื้อสะสมชุดอุปกรณ์ Dota ใหม่ล่าสุดในปีนี้ สั่งซื้อเสื้อแทรคแจ็คเก็ต เสื้องานแข่ง เสื้อฮู้ด หมวก และของใหม่หลากหลายที่มาในรูปแบบ TI...</div>
+                    <div class="news-title">ร้านค้าลับเปิดให้เยี่ยมชมแล้ว</div>
+                    <div class="news-date">7 ตุลาคม 2564</div>
+                </div>
+            </a>
+            <a style="width: 400px; height: 250px; margin: 15px;" href="#">
+                <div class="news" style="background-image: url(&quot;https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/clans/3703047/f0778cd4daa41a7dce1d6e3bf8bbf0a2df81428c.png&quot;);">
+                    <div class="shadow-news"></div>
+                    <div class="news-content-hidden">ด้วยการถ่ายทอดสดใน 5 ภาษา ระบบการเชียร์แบบใหม่หมดจด และการเชื่อมต่อระบบ Twitch ที่ดียิ่งขึ้น — The International เริ่มต้นขึ้นแล้ว </div>
+                    <div class="news-title">The International มาถึงแล้ว</div>
+                    <div class="news-date">7 ตุลาคม 2564</div>
+                </div>
+            </a>
+        </div>
+    </div>
+    <?php include './components/footer.php' ?>
+</body>
+</html>
